1322fd20f537faf70f606c802d0ce5901ea2d5d8
SanjayNarayanan <112078568+SanjayNarayanan@users.noreply.github.com>
2023-12-03 22:47:10 +0530

    Seventh commit

---
+++ sanjaystr.java
@@ -0,0 +1,39 @@
+package practise;
+
+import java.util.Scanner;
+
+public class sanjaystr {
+            // public static void main(String[] args) {
+            //     String[]sr={"sanjay,","ramji","ramjiwife","sanjaywife","sparrow"};
+            //     System.out.println(sr[4]+" "+sr[2]);
+
+            //     for(int i=0;i<sr.length;i++){
+            //         System.out.println(sr[i]);
+            //     }
+            //     System.out.println("----------");
+            //    for(int j=sr.length-1;j>0;j--){
+            //     System.out.println(sr[j]);
+            //    }
+
+            //    for(String a:sr);{
+            //     System.out.println();
+            //    }
+
+                
+            // }  
+
+
+
+            public static void main(String[] args) {
+                 String alphs[]=new String[8];
+                 Scanner sc=new Scanner(System.in);
+                 System.out.println("enter the names");
+
+                 for( int i=0;i<alphs.length;i++){
+                    alphs[i]=sc.next();
+                   
+
+                 }
+
+            }  
+}
